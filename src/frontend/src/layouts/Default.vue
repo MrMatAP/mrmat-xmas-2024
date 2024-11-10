93781683d8d1e7e782cf53e9d23134c70fc20881
Mathieu Imfeld <imfeldma@gmail.com>
2024-11-10 11:58:48 +0100

    Added sideback background

--- src/frontend/src/layouts/Default.vue
+++ src/frontend/src/layouts/Default.vue
@@ -2,9 +2,7 @@
 </script>
 
 <template>
-  <div id="tree">
-    <canvas id="ctree"></canvas>
-  </div>
+  <div id="tree"></div>
   <div id="main">
     <router-view></router-view>
   </div>
@@ -15,11 +13,8 @@
   flex: 1 20vw;
   height: 100vh;
   overflow: hidden;
-}
-
-#ctree {
-  width: 100%;
-  height: 100%;
+  background-image: url("/sidebar.png");
+  background-repeat: repeat-y;
 }
 
 #main {
