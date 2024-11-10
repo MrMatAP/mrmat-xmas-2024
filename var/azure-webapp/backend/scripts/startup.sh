1f283ce65dd6c18c3401f19bc6266773a216d953
Mathieu Imfeld <imfeldma@gmail.com>
2024-11-10 14:53:30 +0100

    Forcing startup on port 8000
    Despite their docs, SERVER_PORT is not set

--- var/azure-webapp/backend/scripts/startup.sh
+++ var/azure-webapp/backend/scripts/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting on port $SERVER_PORT"
-python -m uvicorn mrmat_xmas_2024.app:app --host 0.0.0.0 --port $SERVER_PORT
+python -m uvicorn mrmat_xmas_2024.app:app --host 0.0.0.0 --port 8000
